b2235b5ac7e50d684b958b259cd2d3e98c7c1ce7
haseeb360024 <muhammedhaseeb315@gmail.com>
2023-12-10 15:27:28 +0530

    first

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -72,16 +72,16 @@ const Footer = () => {
           <Col lg="3" md="4" sm="6">
             <div className="mb-4">
               <h5 className="footer__link-title mb-4">Head Office</h5>
-              <p className="office__info">123 Zindabazar, Sylhet, Bangladesh</p>
-              <p className="office__info">Phone: +0995345875365</p>
+              <p className="office__info">Kochi, Kakkanad</p>
+              <p className="office__info">Phone: +91 9744255921</p>
 
-              <p className="office__info">Email: muhib5532@gmail.com</p>
+              <p className="office__info">Email: muhammedhaseeb@gmail.com</p>
 
               <p className="office__info">Office Time: 10am - 7pm</p>
             </div>
           </Col>
 
-          <Col lg="3" md="4" sm="12">
+          <Col lg="3" md=
             <div className="mb-4">
               <h5 className="footer__link-title">Newsletter</h5>
               <p className="section__description">Subscribe our newsletter</p>
@@ -98,7 +98,7 @@ const Footer = () => {
             <div className="footer__bottom">
               <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
                 <i class="ri-copyright-line"></i>Copyright {year}, Developed by
-                Muhibur Rahman. All rights reserved.
+                Muhammed Haseeb. All rights reserved.
               </p>
             </div>
           </Col>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
+import emailjs from '@emailjs/browser';
 
 import "../styles/contact.css";
 
@@ -25,8 +26,26 @@ const socialLinks = [
   },
 ];
 
-const Contact = () => {
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      'service_aif4l07',
+     'template_nuje2mh',
+      form.current,
+       '23sXOTGukUMvuRcfO')
+      .then((result) => {
+          console.log(result.text);
+          console.log("message sent!!");
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
+    
     <Helmet title="Contact">
       <CommonSection title="Contact" />
       <section>
@@ -35,41 +54,36 @@ const Contact = () => {
             <Col lg="7" md="7">
               <h6 className="fw-bold mb-4">Get In Touch</h6>
 
-              <Form>
-                <FormGroup className="contact__form">
-                  <Input placeholder="Your Name" type="text" />
-                </FormGroup>
-                <FormGroup className="contact__form">
-                  <Input placeholder="Email" type="email" />
-                </FormGroup>
-                <FormGroup className="contact__form">
-                  <textarea
-                    rows="5"
-                    placeholder="Message"
-                    className="textarea"
-                  ></textarea>
-                </FormGroup>
-
-                <button className=" contact__btn" type="submit">
-                  Send Message
-                </button>
-              </Form>
+       <form ref={form} onSubmit={sendEmail}>
+      <input className="w-75" placeholder="Your Name" type="text" name="user_name" />
+      <div className="mt-3">
+      <input className="w-75" placeholder="email" type="email" name="user_email" />
+      </div>
+      
+      <textarea  className="mt-3 w-75" name="message" />
+      <div className="btn mt-1"> </div>
+      <button className=" contact__btn " type="submit" value="Send">
+       Send Message
+      </button>
+      
+       </form>
+              
             </Col>
 
             <Col lg="5" md="5">
               <div className="contact__info">
                 <h6 className="fw-bold">Contact Information</h6>
                 <p className="section__description mb-0">
-                  123 ZindaBazar, Sylhet, Bangladesh
+                  malappuram,kerala 676552
                 </p>
                 <div className=" d-flex align-items-center gap-2">
                   <h6 className="fs-6 mb-0">Phone:</h6>
-                  <p className="section__description mb-0">+88683896366</p>
+                  <p className="section__description mb-0">+91 9744255921</p>
                 </div>
 
                 <div className=" d-flex align-items-center gap-2">
                   <h6 className="mb-0 fs-6">Email:</h6>
-                  <p className="section__description mb-0">example@gmail.com</p>
+                  <p className="section__description mb-0">muhammedhaseeb921@gmail.com</p>
                 </div>
 
                 <h6 className="fw-bold mt-4">Follow Us</h6>
